9e190bc4663c255e3ec7c61d3059ea8490b50806
Clifford18 <cliffordjohn.cj@gmail.com>
2022-01-14 14:38:40 +0300

    add JdbcConnector.java file

---
+++ java-backend/src/main/java/JdbcConnector.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+public class JdbcConnector {
+
+
+    public static void main(String[] args){
+
+
+        Connection myConn = null; ;
+        Statement myStatement = null; ;
+        ResultSet myResult = null;
+
+        try {
+
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_management_system","root","Pa55w0rd");
+
+            myStatement = myConn.createStatement();
+
+            myResult = myStatement.executeQuery("select * from tasks");
+
+            while (myResult.next()){
+                System.out.println(myResult.getString("task_id"));
+            }
+            myResult.close();
+            myStatement.close();
+            myConn.close();
+
+            myConn =null; myStatement = null; myResult = null;
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            if(myResult != null) try { myResult.close(); } catch (SQLException ignore) {}
+            if(myStatement != null) try { myStatement.close(); } catch (SQLException ignore) {}
+            if(myConn != null) try { myConn.close(); } catch (SQLException ignore) {}
+
+            myConn = null; myStatement = null; myResult = null;
+        }  finally {
+            if(myResult != null) try { myResult.close(); } catch (SQLException ignore) {}
+            if(myStatement != null) try { myStatement.close(); } catch (SQLException ignore) {}
+            if(myConn != null) try { myConn.close(); } catch (SQLException ignore) {}
+
+            myConn = null; myStatement = null; myResult = null;
+        }
+
+
+    }
+}
